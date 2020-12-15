34487d09d2fa0208814ae9ae2bffaee228341346
acquayefrank <acquayefrank@gmail.com>
2020-12-15 18:31:38 +0300

    completed project .. I think

--- src/misc/bin/indocker/database-creation.sh
+++ src/misc/bin/indocker/database-creation.sh
@@ -3,5 +3,5 @@
 psql -h $DB_HOST -p $DB_INTERNAL_PORT -U $DB_USER -c "SET TIME ZONE 'UTC';" --quiet
 psql -h $DB_HOST -p $DB_INTERNAL_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_DBNAME;" --quiet
 psql -h $DB_HOST -p $DB_INTERNAL_PORT -U $DB_USER -c "CREATE DATABASE $DB_DBNAME ENCODING 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';" --quiet
-psql -h $DB_HOST -p $DB_INTERNAL_PORT -U $DB_USER -c "CREATE DATABASE metabase 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';" --quiet
+psql -h $DB_HOST -p $DB_INTERNAL_PORT -U $DB_USER -c "CREATE DATABASE metabase ENCODING 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';" --quiet
 echo "Database has been created correctly"
